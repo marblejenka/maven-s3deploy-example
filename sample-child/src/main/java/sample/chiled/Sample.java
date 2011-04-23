0849cae27e442ed9c273bb957d8fb690413eb7b6
shingo furuyama <marblejenka@gmail.com>
2011-04-23 20:55:19 +0900

    commit projects.

---
+++ sample-child/src/main/java/sample/chiled/Sample.java
@@ -0,0 +1,14 @@
+package sample.chiled;
+
+public class Sample {
+
+	private String value = null;
+
+	public Sample(String value) {
+		this.value = value;
+	}
+
+	public void print() {
+		System.out.println(value + " hello.");
+	}
+}
---
+++ sample-client/src/main/java/sample/client/Main.java
@@ -0,0 +1,11 @@
+package sample.client;
+
+import sample.chiled.Sample;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Sample sample = new Sample("test");
+		sample.print();
+	}
+}
